eeede7ddff7d0dc0db5fe5fa507794f1494f664d
timefliez1210 <crgabig@gmail.com>
2024-12-12 14:36:32 +0800

    added withdraw logic, added tests and refactored updateBalances() to accomodate withdrawals

--- src/Stable.sol
+++ src/Stable.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.28;
 import {Utils} from "./Utils.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 /**
  * @title Decentralized Stablecoin with mixed over-collateralisation 
@@ -15,6 +16,8 @@ contract Stable is Utils {
     using SafeERC20 for IERC20;
     error NotWhitelistedAsset(address);
     error BelowMinimumDeposit();
+    error AmountExceedsUserBalance(uint256, uint256);
+    error StopDoingWeirdStuff();
 
     mapping(address asset => bool isAllowed) public s_whitelist;
     mapping(address asset => uint256 balance) public s_totalAssetBalances;
@@ -26,6 +29,9 @@ contract Stable is Utils {
         s_whitelist[0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE] = true;
     }
 
+    ////////////////////////////////////////////////////////////////////////////////
+    /////////////////////// External User-Facing Functions /////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
     /**
      * @dev deposit functionality for all whitelisted ERC20s and Ether, also entrypoint for all accounting
      * @param _asset address of the asset to deposit, user 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE for native Ether
@@ -39,24 +45,74 @@ contract Stable is Utils {
             if(msg.value < MIN_DEPOSIT_ETH) {
                 revert BelowMinimumDeposit();
             } else {
-                _updateBalances(msg.sender, _asset, msg.value);
+                _updateBalances(msg.sender, _asset, int256(msg.value));
             }
         } else {
             IERC20(_asset).safeTransferFrom(msg.sender, address(this), _amount);
-            _updateBalances(msg.sender, _asset, _amount);
+            _updateBalances(msg.sender, _asset, int256(_amount));
         }
     }
 
+    function withdraw(address _asset, uint _amount) external nonReentrant {
+        /*
+         * @Todo if theres colleteral attached we need to check Health factor etc.
+         */
+        // Checks
+        if(_amount > s_userBalances[msg.sender][_asset]) {
+            revert AmountExceedsUserBalance(_amount, s_userBalances[msg.sender][_asset]);
+        }
+        if(_asset == ETHER) {
+            // Effects
+            int256 amount = -int256(_amount); 
+            _updateBalances(msg.sender, _asset, amount);
+            address payable to = payable(msg.sender);
+            require(to != address(0), "conversion went wrong!");
+            // Interactions
+            (bool success, ) = to.call{value: _amount}("");
+            require(success, "transfer failed!");
+        } else {
+            // Effects
+            int256 amount = -int256(_amount); 
+            _updateBalances(msg.sender, _asset, amount);
+            // Interaction
+            IERC20(_asset).safeTransfer(msg.sender, _amount);
+        } 
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    /////////////////////// External Priviledged Functions /////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
     function whitelistTokens(address _token) external onlyOwner {
         s_whitelist[_token] = true;
     }
 
-    function isWhitelisted(address _token) external view returns(bool) {
+    ////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////// Public Getter Functions //////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    function isWhitelisted(address _token) public view returns(bool) {
         return s_whitelist[_token];
     }
     
-    function _updateBalances(address _user, address _asset, uint256 _amount) internal {
-        s_totalAssetBalances[_asset] += _amount;
-        s_userBalances[_user][_asset] += _amount;
+    function getTotalBalance(address _asset) public view returns(uint256) {
+        return s_totalAssetBalances[_asset];
+    }
+
+    function getUserBalance(address _user, address _asset) public view returns (uint256) {
+        return s_userBalances[_user][_asset];
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////// Internal Helper Functions ////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////
+    function _updateBalances(address _user, address _asset, int256 _amount) internal {
+        if(_amount < 0) {
+            uint256 amount = uint256(-_amount);
+            s_totalAssetBalances[_asset] -= amount;
+            s_userBalances[_user][_asset] -= amount;
+        } else {
+            uint256 amount = uint256(_amount);
+            s_totalAssetBalances[_asset] += amount;
+            s_userBalances[_user][_asset] += amount;
+        }
     }
 }
--- test/StableTest.t.sol
+++ test/StableTest.t.sol
@@ -5,6 +5,13 @@ pragma solidity 0.8.28;
 import {BaseTest} from "./utils/BaseTest.t.sol";
 
 contract StableTest is BaseTest {
+
+    function test_whitelistNotOwner() public {
+        vm.startPrank(users[1]);
+        vm.expectRevert();
+        stable.whitelistTokens(address(weth));
+        vm.stopPrank();
+    }
     function test_fuzz_depositEtherSuccess(uint256 amount) public {
         vm.assume(amount <= 10e18);
         vm.assume(amount > MIN_DEPOSIT_ETH);
@@ -71,10 +78,86 @@ contract StableTest is BaseTest {
         assertEq(weth.balanceOf(address(stable)), amount);
     }
 
-    function test_whitelistNotOwner() public {
-        vm.startPrank(users[1]);
-        vm.expectRevert();
-        stable.whitelistTokens(address(weth));
+    function test_fuzz_withdrawSuccess(uint256 i, uint256 amountERC20, uint256 amountEther) public {
+        i = bound(i, 1, 4);
+        amountERC20 = bound(amountERC20, 1, 10e18);
+        amountEther = bound(amountEther, MIN_DEPOSIT_ETH, 10e18);
+        whitelistTokens();
+        vm.startPrank(users[i]);
+        stable.deposit(address(weth), amountERC20);
+        (bool success,) = address(stable).call{value: amountEther}(abi.encodeWithSignature("deposit(address,uint256)", ETHER, 0));
         vm.stopPrank();
+        assertEq(weth.balanceOf(users[i]), 10e18 - amountERC20);
+        assertEq(stable.getTotalBalance(address(weth)), amountERC20);
+        assertEq(stable.getUserBalance(users[i], address(weth)), amountERC20);
+        assertEq(users[i].balance, 10e18 - amountEther);
+        assertEq(stable.getTotalBalance(ETHER), amountEther);
+        assertEq(stable.getUserBalance(users[i], ETHER), amountEther);
+        vm.startPrank(users[i]);
+        stable.withdraw(ETHER, amountEther);
+        stable.withdraw(address(weth), amountERC20);
+        vm.stopPrank();
+        assertEq(weth.balanceOf(users[i]), 10e18);
+        assertEq(stable.getTotalBalance(address(weth)), 0);
+        assertEq(stable.getUserBalance(users[i], address(weth)), 0);
+        assertEq(users[i].balance, 10e18);
+        assertEq(stable.getTotalBalance(ETHER), 0);
+        assertEq(stable.getUserBalance(users[i], ETHER), 0);
+    }
+    
+    function test_fuzz_withdrawFailExceedsBalanceERC20(uint256 i, uint256 amountERC20, uint256 amountEther) public {
+        i = bound(i, 1, 4);
+        amountERC20 = bound(amountERC20, 1, 10e18);
+        amountEther = bound(amountEther, MIN_DEPOSIT_ETH, 10e18);
+        whitelistTokens();
+        vm.startPrank(users[i]);
+        stable.deposit(address(weth), amountERC20);
+        (bool success,) = address(stable).call{value: amountEther}(abi.encodeWithSignature("deposit(address,uint256)", ETHER, 0));
+        vm.stopPrank();
+        assertEq(weth.balanceOf(users[i]), 10e18 - amountERC20);
+        assertEq(stable.getTotalBalance(address(weth)), amountERC20);
+        assertEq(stable.getUserBalance(users[i], address(weth)), amountERC20);
+        assertEq(users[i].balance, 10e18 - amountEther);
+        assertEq(stable.getTotalBalance(ETHER), amountEther);
+        assertEq(stable.getUserBalance(users[i], ETHER), amountEther);
+        vm.startPrank(users[i]);
+        stable.withdraw(ETHER, amountEther);
+        vm.expectRevert();
+        stable.withdraw(address(weth), amountERC20 +5);
+        vm.stopPrank();
+        assertEq(weth.balanceOf(users[i]), 10e18 - amountERC20);
+        assertEq(stable.getTotalBalance(address(weth)), amountERC20);
+        assertEq(stable.getUserBalance(users[i], address(weth)), amountERC20);
+        assertEq(users[i].balance, 10e18);
+        assertEq(stable.getTotalBalance(ETHER), 0);
+        assertEq(stable.getUserBalance(users[i], ETHER), 0);
+    }
+
+    function test_fuzz_withdrawFailExceedsBalanceEther(uint256 i, uint256 amountERC20, uint256 amountEther) public {
+        i = bound(i, 1, 4);
+        amountERC20 = bound(amountERC20, 1, 10e18);
+        amountEther = bound(amountEther, MIN_DEPOSIT_ETH, 10e18);
+        whitelistTokens();
+        vm.startPrank(users[i]);
+        stable.deposit(address(weth), amountERC20);
+        (bool success,) = address(stable).call{value: amountEther}(abi.encodeWithSignature("deposit(address,uint256)", ETHER, 0));
+        vm.stopPrank();
+        assertEq(weth.balanceOf(users[i]), 10e18 - amountERC20);
+        assertEq(stable.getTotalBalance(address(weth)), amountERC20);
+        assertEq(stable.getUserBalance(users[i], address(weth)), amountERC20);
+        assertEq(users[i].balance, 10e18 - amountEther);
+        assertEq(stable.getTotalBalance(ETHER), amountEther);
+        assertEq(stable.getUserBalance(users[i], ETHER), amountEther);
+        vm.startPrank(users[i]);
+        vm.expectRevert();
+        stable.withdraw(ETHER, amountEther + 1);
+        stable.withdraw(address(weth), amountERC20);
+        vm.stopPrank();
+        assertEq(weth.balanceOf(users[i]), 10e18);
+        assertEq(stable.getTotalBalance(address(weth)), 0);
+        assertEq(stable.getUserBalance(users[i], address(weth)), 0);
+        assertEq(users[i].balance, 10e18 - amountEther);
+        assertEq(stable.getTotalBalance(ETHER), amountEther);
+        assertEq(stable.getUserBalance(users[i], ETHER), amountEther);
     }
 }
